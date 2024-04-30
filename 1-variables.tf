95ab1658b1e9fa926f30730176f123c04ed83ac7
Sathia Saithanya <sathimal@amazon.com>
2024-04-30 16:52:54 +0800

    First Commit

---
+++ 1-variables.tf
@@ -0,0 +1,19 @@
+
+variable "account_id" {
+  type    = string
+  default = "740024244647"
+}
+
+variable "region" {
+  type = string
+}
+
+variable "project_name" {
+  type    = string
+  default = "ivs-inspector"
+}
+
+variable "environment" {
+  type    = string
+  default = "ivs"
+}
